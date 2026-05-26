4645b2c1e8d80b5980d5677ab3158003b7077719
GKB-47 <gopalkrishnab@gmail.com>
2026-05-27 00:39:04 +0530

    change in Model again

--- backend/rag.py
+++ backend/rag.py
@@ -99,7 +99,7 @@ def ask_question(question, pdf_text=""):
 
         completion = client.chat.completions.create(
 
-            model="llama-3.1-8b-instant",
+            model="llama-3.3-70b-versatile",
 
             messages=[
                 {
