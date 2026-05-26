f5b059ed0fa657aadcc925943f476967d0ddbe58
GKB-47 <gopalkrishnab@gmail.com>
2026-05-26 16:35:16 +0530

    modified API URL

--- frontend/app.py
+++ frontend/app.py
@@ -165,14 +165,25 @@ if prompt:
                     )
                 }
 
+            # ======================================================
+            # BACKEND API
+            # ======================================================
+
+            API_URL = "https://cloudinvent-backend.onrender.com/chat"
+            #API_URL = "http://localhost:8000/chat"
+            #API_URL = "http://127.0.0.1:8000/chat"
+
             response = requests.post(
-                "http://127.0.0.1:8000/chat",
-                data={
-                    "question": prompt
-                },
-                files=files,
-                timeout=120
+
+            API_URL,
+
+            json={
+            "question": prompt
+            },
+
+            timeout=120
             )
+
 
             answer = response.json()["answer"]
 
